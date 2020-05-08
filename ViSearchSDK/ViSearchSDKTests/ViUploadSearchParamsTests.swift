618e690d470ffb6c9e04341738e660a2ac6dc828
Hung <hung@visenze.com>
2020-05-08 13:35:39 +0800

    tests

--- ViSearchSDK/ViSearchSDKTests/ViUploadSearchParamsTests.swift
+++ ViSearchSDK/ViSearchSDKTests/ViUploadSearchParamsTests.swift
@@ -25,6 +25,33 @@ class ViUploadSearchParamsTests: XCTestCase {
         XCTAssertNil( ViUploadSearchParams(im_id: "") , "Missing image id")
         XCTAssertNil( ViUploadSearchParams(im_url: "") , "Missing image url")
         
+        /*
+        let expectation = self.expectation(description: "wait_for_response")
+        
+        ViSearch.sharedInstance.setup(appKey: "")
+        let params = ViUploadSearchParams(im_url: "https://imgresize.visenze.com.cn/6163/512x512/VIP-CN_2165183117.jpeg")
+        params?.score = true
+        let p = ViPoint(x: 100, y: 100)
+        params?.points = [p]
+        
+        ViSearch.sharedInstance.discoverSearch(params: params!,
+        successHandler: {
+            (data : ViResponseData?) -> Void in
+            // Do something when request succeeds
+            // preview by calling : dump(data)
+            // check ViResponseData.hasError and ViResponseData.error for any errors return by ViSenze server
+            dump(data)
+            expectation.fulfill()
+            
+        },
+        failureHandler: {
+            (err) -> Void in
+            // Do something when request fails e.g. due to network error
+            print ("error: \\(err.localizedDescription)")
+            expectation.fulfill()
+        })
+        
+        waitForExpectations(timeout: 30, handler: nil) */
     }
     
 }
